a38f4e9be428c78a2e17787fc822c92c46a156af
kenny_nobody <nobodykenny@gmail.com>
2020-08-22 03:13:57 +0500

    'work'

---
+++ src/blocks/modules/news-users/news-users.js
@@ -0,0 +1,13 @@
+modules.define('news-users', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
---
+++ src/blocks/modules/title-h2/title-h2.js
@@ -0,0 +1,13 @@
+modules.define('title-h2', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
