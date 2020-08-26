b6e94e22919e6986179870245ea6d35f525e1e6e
kenny_nobody <nobodykenny@gmail.com>
2020-08-26 08:58:06 +0500

    'work'

--- src/blocks/modules/file/file.js
+++ src/blocks/modules/file/file.js
@@ -4,10 +4,32 @@
 //     onSetMod: {
 //         js: {
 //             inited: function() {
-                
+
 //             }
 //         }
 //     }
 // }));
 
 // });
+
+(function toggleFileInputs(){
+	const block = document.querySelectorAll('.file');
+	for (let i = 0; i < block.length; i++ ) {
+		addListener(block[i]);
+	}
+})();
+
+function addListener(block) {
+	let text = block.querySelector('.file__text');
+	let input = block.querySelector('.file__field');
+
+	input.addEventListener('change', function(){
+		changeState(text, input);
+	});
+}
+
+function changeState(text, input) {
+	let message = input.files[0].name;
+	text.textContent = message;
+	// console.dir(input);
+}
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -4,6 +4,7 @@ import "%modules%/footer/footer";
 // import "%modules%/slider-header/slider-header";
 import "%modules%/faq-item/faq-item";
 import "%modules%/mmenu/mmenu";
+import "%modules%/file/file";
 import "%modules%/slider-main/slider-main";
 import "%modules%/slider-bottom/slider-bottom";
 import "%modules%/gallery-item/gallery-item";
