3391d8d660b0045b2a3433966a7b101ac08fa354
kenny_nobody <nobodykenny@gmail.com>
2020-08-28 12:35:11 +0500

    'work'

--- dist/js/main.js
+++ dist/js/main.js
@@ -285,7 +285,39 @@ $('[data-fancybox="gallery"]').fancybox({
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+(function initSearchToggle() {
+  var btnOpen = document.querySelector('.header__open-search');
+  var btnClose = document.querySelector('.head-search__btn--close');
+  var block = document.querySelector('.head-search');
 
+  if (btnOpen && block) {
+    btnOpen.addEventListener('click', open);
+  }
+
+  if (btnClose && block) {
+    btnClose.addEventListener('click', close);
+  }
+
+  if (block) {
+    window.addEventListener('resize', close);
+  }
+
+  function open() {
+    if (window.innerWidth < 1200) {
+      block.classList.add('head-search--open');
+    } else {
+      block.classList.remove('head-search--open');
+    }
+  }
+
+  function close(e) {
+    e.preventDefault();
+
+    if (btnClose && block && window.innerWidth < 1200) {
+      block.classList.remove('head-search--open');
+    }
+  }
+})();
 
 /***/ }),
 
---
+++ src/blocks/modules/header/header.js
@@ -0,0 +1,33 @@
+(function initSearchToggle(){
+	const btnOpen = document.querySelector('.header__open-search');
+	const btnClose = document.querySelector('.head-search__btn--close');
+	const block = document.querySelector('.head-search');
+
+	if (btnOpen && block) {
+		btnOpen.addEventListener('click', open);
+	}
+
+	if (btnClose && block) {
+		btnClose.addEventListener('click', close);
+	}
+	
+	if (block) {
+		window.addEventListener('resize', close);
+	}
+	
+	function open() {
+		if (window.innerWidth < 1200) {
+			block.classList.add('head-search--open');
+		} else {
+			block.classList.remove('head-search--open');
+		}
+	}
+
+	function close(e) {
+		e.preventDefault();
+		if (btnClose && block && window.innerWidth < 1200) {
+			block.classList.remove('head-search--open');
+		}
+	}
+	
+})();
