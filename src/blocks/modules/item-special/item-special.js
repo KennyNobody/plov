b0a8c57a1f08d6359915a4279679e00d3b967c73
Kenny <nobodykenny@gmail.com>
2021-04-06 20:00:33 +0300

    'added_item_special'

---
+++ src/blocks/modules/item-special/item-special.js
@@ -0,0 +1,13 @@
+modules.define('item-special', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
